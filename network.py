379257313c817795ac460dd2115864c7782bb370
glample <glample@cs.cmu.edu>
2015-10-18 14:11:30 -0400

    add GRU network

--- network.py
+++ network.py
@@ -164,3 +164,81 @@ class LSTM(object):
         self.output = h[-1]
 
         return self.output
+
+
+class GRU(object):
+    """
+    Gated Recurrent Units (GRU). Can be used with or without batches.
+    Without batches:
+        Input: matrix of dimension (sequence_length, input_dim)
+        Output: vector of dimension (output_dim)
+    With batches:
+        Input: tensor3 of dimension (sequence_length, batch_size, input_dim)
+        Output: matrix of dimension (batch_size, output_dim)
+    """
+
+    def __init__(self, input_dim, hidden_dim, with_batch=True, name='LSTM'):
+        """
+        Initialize neural network.
+        """
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.with_batch = with_batch
+        self.name = name
+
+        # Domain range for weights initialization
+        drange_x = get_drange((input_dim, hidden_dim))
+        drange_h = get_drange((hidden_dim, hidden_dim))
+
+        # Update gate weights
+        self.w_z = create_shared(drange_x * random_weights((input_dim, hidden_dim)), name + '__w_z')
+        self.u_z = create_shared(drange_h * random_weights((hidden_dim, hidden_dim)), name + '__u_z')
+
+        # Reset gate weights
+        self.w_r = create_shared(drange_x * random_weights((input_dim, hidden_dim)), name + '__w_r')
+        self.u_r = create_shared(drange_h * random_weights((hidden_dim, hidden_dim)), name + '__u_r')
+
+        # New memory content weights
+        self.w_n = create_shared(drange_x * random_weights((input_dim, hidden_dim)), name + '__w_n')
+        self.u_n = create_shared(drange_h * random_weights((hidden_dim, hidden_dim)), name + '__u_n')
+
+        # Initialize the bias vector, h_0, to the zero vector
+        self.h_0 = create_shared(np.zeros((hidden_dim,)), name + '__h_0')
+
+        # Define parameters
+        self.params = [self.w_z, self.u_z,
+                       self.w_r, self.u_r,
+                       self.w_n, self.u_n,
+                       self.h_0]
+
+    def link(self, input):
+        """
+        Propagate the input through the network and return the last hidden vector.
+        The whole sequence is also accessible through self.h
+        """
+
+        def recurrence(x_t, h_tm1):
+            z_t = T.nnet.sigmoid(T.dot(x_t, self.w_z) + T.dot(h_tm1, self.u_z))
+            r_t = T.nnet.sigmoid(T.dot(x_t, self.w_r) + T.dot(h_tm1, self.u_r))
+            n_t = T.tanh(T.dot(x_t, self.w_n) + r_t * T.dot(h_tm1, self.u_n))
+            h_t = z_t * h_tm1 + (1 - z_t) * n_t
+            return h_t
+
+        # If we used batches, we have to permute the first and second dimension.
+        if self.with_batch:
+            self.input = input.dimshuffle(1, 0, 2)
+            outputs_info = T.alloc(self.h_0, self.input.shape[1], self.hidden_dim)
+        else:
+            self.input = input
+            outputs_info = self.h_0
+
+        h, _ = theano.scan(
+            fn=recurrence,
+            sequences=self.input,
+            outputs_info=outputs_info,
+            n_steps=self.input.shape[0]
+        )
+        self.h = h
+        self.output = h[-1]
+
+        return self.output
